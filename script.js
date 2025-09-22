38373c2568505fc9e93019ecba5559c52a3d6bc0
alexisanunez <alexisarielnunez@gmail.com>
2025-09-22 14:27:11 -0300

    padding correct and image rework

---
+++ script.js
@@ -0,0 +1,16 @@
+// Cargar el header en el div con id="navbar"
+fetch("header.html")
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById("navbar").innerHTML = data;
+    });
+
+// Insertar footer automáticamente
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("footer.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("footer").innerHTML = data;
+        })
+        .catch(error => console.error("Error cargando el footer:", error));
+});
